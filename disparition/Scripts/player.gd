10551ec169f5b65db3ca4c7f511e65f29c710784
Célia D'Agostini <celidago57@gmail.com>
2025-08-02 15:20:16 +0200

    Player + mouvements de base

---
+++ disparition/Scripts/player.gd
@@ -0,0 +1,19 @@
+class_name PlayerBody extends CharacterBody2D
+
+const SPEED = 300.0
+
+
+func _physics_process(delta: float) -> void:
+	
+	var direction_x = Input.get_axis("Gauche","Droite")
+	var direction_y = Input.get_axis("Haut","Bas")
+	
+	var direction = Vector2(direction_x,direction_y).normalized()
+	if direction:
+		velocity=direction*SPEED
+	else :
+		velocity.x = move_toward(velocity.x,0,SPEED)
+		velocity.y = move_toward(velocity.y,0,SPEED)
+
+
+	move_and_slide()
